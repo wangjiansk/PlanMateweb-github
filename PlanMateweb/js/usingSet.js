70f076acc751ab608938598ccdab4d6d3f8b1849
y-s-y-979 <1434706848@qq.com>
2019-04-28 14:33:19 +0800

    上传用户设置界面代码

--- PlanMateweb/js/usingSet.js
+++ PlanMateweb/js/usingSet.js
@@ -1,117 +1,48 @@
-;(function ($) {
-    let text = {
-        init:function() {
-            this.bindEvent()
-            this.clientW = $(window).height()
-            this.render();
-            console.log(this)
+(function ($) {
+    let useSeting = {
+        init() {
+            this.nav = $('#navContent').find('li');
+            this.child = $(".navList").find('ul');
+            this.childNav =this.child.find('li');
+            this.inputGroup = $('.inputGroup');
+            this.btn = $('.show');
+            this.bind();
         },
-        bindEvent:function() {
-            let nav = $('.nav').find('li')
 
-let flag = true;
-            nav.each(function(index, ele) {
-                let self = this
+
+        bind() {
+            let self =this;
+            this.childNav.each(function (index, ele) {
                 $(ele).on('click', function () {
-                    if(flag){
-                        flag = false;
-                        let inpContainer = $('.MainContent').find('.inpGroup').find('.inpContainer')
-                        $('.active').removeClass('active')
-                        $(this)
-                            .find('a')
-                            .addClass('active')
-                        $('.innerActive')
-                            .removeClass('innerActive')
-                            .slideUp('slow')
-                        inpContainer
-                            .eq(index)
-                            .slideDown('slow')
-                            .addClass('innerActive');
-                        setTimeout(function() {
-                            flag = true;
-                        }, 500);
-                    }
+                    if($(ele)){}
+                    $('.childActive').removeClass('childActive');
+                    $(this).find('a').addClass('childActive');
+                    $('.innerActive').removeClass('innerActive');
+                    self.inputGroup.eq(index).addClass('innerActive')
+                })
 
+            });
+            this.nav.each(function (index,ele) {
+                $(ele).on('click',function () {
+                    $('.active').removeClass('active');
+                    $(this).find('a').addClass('active');
+                    self.child.eq(index).css({display:'none'});
+                    self.child.eq(index).css({display:'block'});
                 })
             });
-            $('.navbar-header').find('.show').on('click', function () {
-                if(flag){
-                    flag = false;
-                    if (!$(this).hasClass('showActive')) {
-                        $(this).addClass('showActive').parent().prev().css({display: 'block'});
-                        setTimeout(function(){flag = true;},500)
-                    }
-                    else {
-                        $(this).removeClass('showActive').prev().css({display: 'none'});
-                        setTimeout(function(){flag = true;},500)
-                    }
+            this.btn.on('click',function () {
+                $(this).toggleClass("open");
+                if($(this).hasClass('open')){
+                    $(this).addClass('showActive');
+                    $('#bs-example-navbar-collapse-1').addClass('isopen');
+                }else{
+                    $(this).removeClass('showActive')
                 }
-
-
-            })
-        },
-        render:function() {
-            let inpGroup = $('.inpGroup');
-            inpGroup.css({
-                backgroundImage: 'url(../Image/bg.jpg)',
-                backgroundPosition: 'center center',
-                backgroundSize: 'cover',
-                opacity: '.8'
             });
-            $('.inpGroup').inpGroup({
-                title: '员工新建',
-                label: ['名字', '密码','邮箱','电话'],
-                inputg: ['text', 'password','email','tel'],
-                inputId: ['name', 'password','email','phone'],
-                btnName: '确认新建',
-                btnClass: 'btn-success'
-            }).inpGroup({
-                title: '员工修改',
-                label: ['名字', '密码','是否登陆'],
-                inputg: ['text', 'password','checkbox'],
-                inputId: ['o', 'p','r'],
-                btnName: '确认修改',
-                btnClass: 'btn-primary'
-            }).inpGroup({
-                title: '员工删除',
-                label: ['名字', '密码'],
-                inputg: ['text', 'passworld'],
-                inputId: ['a', 'b'],
-                btnName: '确认删除',
-                btnClass: 'btn-danger'
-            }).inpGroup({
-                title: '用户组新建',
-                label: ['名字', '密码'],
-                inputg: ['text', 'passworld'],
-                inputId: ['c', 'd'],
-                btnName: '确认新建',
-                btnClass: 'btn-success'
-            }).inpGroup({
-                title: '用户组修改',
-                label: ['名字', '密码'],
-                inputg: ['text', 'passworld'],
-                inputId: ['e', 'f'],
-                btnName: '确认删除',
-                btnClass: 'btn-primary'
-            }).inpGroup({
-                title: '用户组删除',
-                label: ['名字', '密码'],
-                inputg: ['text', 'passworld'],
-                inputId: ['g', 'h'],
-                btnName: '确认删除',
-                btnClass: 'btn-danger'
-            }).inpGroup({
-                title: '用户权限设置',
-                label: ['名字', '密码'],
-                inputg: ['text', 'passworld'],
-                inputId: ['i', 'j'],
-                btnName: '设置',
-                btnClass: 'btn-primary'
-            });
-            $('.inpGroup').find('.inpContainer').eq(0).addClass('innerActive').css({
-                display:'block'
+            $('.isopen').find('ul').find('li').find('a').on('click',function () {
+
             })
         }
-    }
-    text.init()
-})(jQuery)
+    };
+    useSeting.init();
+})(jQuery);

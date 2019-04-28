7809abe50f04b33e18c42556d71fc8d436605270
y-s-y-979 <1434706848@qq.com>
2019-04-28 14:46:41 +0800

    上传用户设置界面代码

--- PlanMateweb/js/usingSet.js
+++ PlanMateweb/js/usingSet.js
@@ -1,9 +1,13 @@
 (function ($) {
     let useSeting = {
         init() {
+            //导航条
             this.nav = $('#navContent').find('li');
+            //左边的导航条
             this.child = $(".navList").find('ul');
+            //左边的导航条内容
             this.childNav =this.child.find('li');
+            //网页内容
             this.inputGroup = $('.inputGroup');
             this.btn = $('.show');
             this.bind();
@@ -39,9 +43,9 @@
                     $(this).removeClass('showActive')
                 }
             });
-            $('.isopen').find('ul').find('li').find('a').on('click',function () {
-
-            })
+            // $('.isopen').find('ul').find('li').find('a').on('click',function () {
+            //
+            // })
         }
     };
     useSeting.init();
